83daf026b13fd2e993ce6152f4a139ca09cc9a8f
vasy <kitten20811@gmail.com>
2020-04-26 18:19:42 +0300

    section 2

--- App.js
+++ App.js
@@ -1,19 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React ,{useState}from 'react'
+import { StyleSheet, Text, View, ScrollView, FlatList  } from 'react-native'
+import {Navbar} from './src/Navbar'
+import { AddTodo } from './src/AddTodo';
+import { Todo } from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    // const newTodo = {
+    //   id: Date.now().toString(),
+    //   title: title
+    // }
+
+    // setTodos((prevTodos) => {
+    //   return [
+    //     ...prevTodos,
+    //     newTodo
+    //   ]
+    // })
+    setTodos(prev => [...prev,{
+      id: Date.now().toString(),
+      title
+    }] )
+  }
+
+  const removeTodo = (todoId) => {
+    setTodos(prev => prev.filter(todo => todo.id !== todoId))
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View >
+      <Navbar title="Todo App !"/>
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo}/>
+
+
+        <FlatList 
+          keyExtractor={item => item.id.toString()}
+          data={todos}
+          renderItem={({item}) => (
+            <Todo todo={item} onRemove={removeTodo} />
+          )}      
+        />
+
+{/* 
+        <View>
+          {todos.map(todo => <Todo todo={todo} key={todo.id} />)}                
+        </View> */}
+      </View>     
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    paddingHorizontal: 30,
+    paddingVertical: 20
+  }
 });
